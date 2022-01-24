44e781fd712dcb1269d6a16ce128460d4b3faaf1
Yavin <yavin@nay.am>
2022-01-24 14:01:14 +0530

    Added Header

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,8 +1,10 @@
 import { signOut } from "next-auth/react";
+import Header from "../components/Header";
 
 export default function Home() {
   return (
     <div>
+      <Header />
       Hello!
       <button onClick={signOut}>Sign Out</button>
     </div>
